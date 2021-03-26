3201e102645fe623fc73fbb00aa38a4dfd1774f5
second1996 <vital10012@gmail.com>
2021-03-26 11:06:49 +0200

    Final home page

--- app/js/_scripts.js
+++ app/js/_scripts.js
@@ -1,26 +1,5 @@
 /**
  *-------------------------------------------------------------------------------------------------------------------------------------------
- * Go up button
- *-------------------------------------------------------------------------------------------------------------------------------------------
-*/
-var go_up_btn = $('#go-up-button')
-
-$(window).on('scroll', function() {
-	if ($(window).scrollTop() > 1000) {
-		go_up_btn.addClass('_is-shown')
-	} else {
-		go_up_btn.removeClass('_is-shown')
-	}
-})
-
-go_up_btn.on('click', function(e) {
-	e.preventDefault()
-	$('html, body').animate({scrollTop:0}, 1000)
-})
-
-
-/**
- *-------------------------------------------------------------------------------------------------------------------------------------------
  * Smooth scroll
  *-------------------------------------------------------------------------------------------------------------------------------------------
 */
@@ -84,12 +63,13 @@ $('#classroomModal').on('show.bs.modal', function (event) {
 	const modal = $(this)
 
 	if (recipient) {
-		modal.find('#classroom-request').removeClass('d-none')
-		modal.find('#classroom-request').addClass('d-block')
+		modal.find('#classroom-request').removeAttr('hidden')
+		modal.find('#classroom-choose').attr('hidden', true)
 		modal.find('#classroom-request .form-control').val(recipient)
+		modal.find('#classroom-choose .form-select option:first').prop('selected', true)
 	} else {
-		modal.find('#classroom-request').removeClass('d-block')
-		modal.find('#classroom-request').addClass('d-none')
+		modal.find('#classroom-request').attr('hidden', true)
 		modal.find('#classroom-request .form-control').removeAttr('value')
+		modal.find('#classroom-choose').removeAttr('hidden')
 	}
 })
